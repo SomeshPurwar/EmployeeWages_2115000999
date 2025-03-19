95f457aa88cbe60d873a50f7795922209f4ffabb
SomeshPurwar <somesh.purwar20@gmail.com>
2025-03-20 01:24:06 +0530

    [Somesh] merge:merged UC3

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,6 +1,6 @@
 //UC1 Checking EMployee Presence
 const IS_Absent=0;
-let empCheck=Math.floor(Math.random()*10)%2;
+let empCheck=Math.floor(Math.random()*10)%3;
 if(empCheck==IS_Absent){
     console.log("Employee is Absent");
     return;
@@ -15,20 +15,26 @@ const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
 
-let empHrs=0;
-empCheck=Math.floor(Math.random()*10)%3;
-switch(empCheck){
-    case IS_PART_TIME:
-        empHrs=PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs=FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs=0;
+// UC3 Refractoring Code 
+
+function getWorkingHours(empcheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+            break;
+        default:
+            return 0;
+    }
 }
 
+let empHrs=0;
+empHrs= getWorkingHours(empCheck);
 let empWage=empHrs*WAGE_PER_HOUR;
-console.log("Emp Wages:"+empWage);
+console.log("Emp Wages:"+ empWage);
+
+
 
 
